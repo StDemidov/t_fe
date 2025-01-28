946ce5b11e73383798c37fe1419cc399c150aa6f
DemidovST Mac <demidov.stepan.py@gmail.com>
2025-01-28 17:49:17 +0300

    Campaigns 2

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,2 +1,2 @@
-// export const hostName = 'https://t-prjct.ru';
-export const hostName = 'http://127.0.0.1:8000';
+export const hostName = 'https://t-prjct.ru';
+// export const hostName = 'http://127.0.0.1:8000';
