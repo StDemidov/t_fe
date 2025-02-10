f2f7b69482c4e2c082223be84a7f7e652b1e44eb
DemidovST Mac <demidov.stepan.py@gmail.com>
2025-02-10 17:37:46 +0300

    hostFix

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,3 +1,3 @@
-// export const hostName = 'https://t-prjct.ru';
-export const hostName = 'http://127.0.0.1:8000';
+export const hostName = 'https://t-prjct.ru';
+// export const hostName = 'http://127.0.0.1:8000';
 /////
